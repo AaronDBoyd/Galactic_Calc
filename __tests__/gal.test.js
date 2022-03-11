8e31b62046f94ffb90f3d7880058e55a8c2bcbbc
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-10 23:36:22 -0800

    fail second test

--- __tests__/gal.test.js
+++ __tests__/gal.test.js
@@ -6,6 +6,14 @@ describe('Traveler', () => {
     const astronaut = new Traveler(34, "Earth"); 
     expect(astronaut.age).toEqual(34);
     expect(astronaut.planet).toEqual("Earth");
-    
+  });
+
+  describe('Travelr#dogYears', () => {
+
+    test("should create a prototype method for determining a user's age based on the value 'Earth' for the planet property", () => {
+      const astronaut = new Traveler(34, "Earth"); 
+      expect(astronaut.planet).toEqual("Earth");
+      expect(astronaut.dogYears()).toEqual(34);
+    })
   });
 });
