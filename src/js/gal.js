a3470619c66824606f0e98701d38a1e391e8271b
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-11 22:05:36 -0800

    pass twelfth test

--- src/js/gal.js
+++ src/js/gal.js
@@ -34,6 +34,8 @@ export default class Traveler {
       daysNumbered = expect / 0.62;
     } else if (this.planet === "Mars") {
       daysNumbered = expect / 1.88;
+    } else if (this.planet === "Jupiter") {
+      daysNumbered = expect / 11.86;
     }
     return parseFloat(daysNumbered.toFixed(2));
   }
