74202d245d28b2e8ea05316fe4481dace9fb2e1d
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-11 19:47:26 -0800

    pass sixth test

--- src/js/gal.js
+++ src/js/gal.js
@@ -15,6 +15,8 @@ export default class Traveler {
       currentAge = this.age / 0.62;
     } else if (this.planet === "Mars") {
       currentAge = this.age / 1.88;
+    } else if (this.planet === "Jupiter") {
+      currentAge = this.age / 11.86;
     }
     return parseFloat(currentAge.toFixed(2))
   }
